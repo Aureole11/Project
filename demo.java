10172a879511d063e0a4edd52dda93ed99d23e05
Purva Gupta <86328971+Aureole11@users.noreply.github.com>
2022-08-02 20:52:26 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;  
-public class TableExample  
-{  
 public static void main(String[] args)   
 {  
 Scanner sc = new Scanner(System.in);  
@@ -11,4 +9,3 @@ for(int i=1; i <= 10; i++)
 System.out.println(num+" * "+i+" = "+num*i);  
 }  
 }  
-}
