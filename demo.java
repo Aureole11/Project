8fdd901c49bda1e67bb1e9f3280a770291d40fb7
Aureole11 <purvagupta5555@gmail.com>
2022-08-02 17:58:05 +0530

    i added demo.java

---
+++ demo.java
@@ -0,0 +1,10 @@
+int i,k;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the Number for the table: ");
+
+int n = sc.nextInt();
+for (i=1;i<=10;i++){
+k = n * i;
+//n + "*" + c + " = " + (n*c))
+System.out.println(n + "*"+ i + " = " + k);	
+}
