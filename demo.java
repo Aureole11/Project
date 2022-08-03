42245247af00107e1dc611206f09c4c8ed611a0d
Purva Gupta <86328971+Aureole11@users.noreply.github.com>
2022-08-03 10:45:17 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -4,12 +4,16 @@ public class Table
 {  
 public static void main(String[] args)   
 {  
+try {
 Scanner sc = new Scanner(System.in);  
 System.out.print("Enter number: ");         
 int num=sc.nextInt();  
 for(int i=1; i <= 10; i++)  
 {        
 System.out.println(num+" * "+i+" = "+num*i);  
+} 
+catch (Exception e) {
+  System.out.println(e);
 }  
-}  
+}
 }
