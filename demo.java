67e4f5a7a105cce20b75b617a0b7a98ac220b02f
Purva Gupta <86328971+Aureole11@users.noreply.github.com>
2022-08-03 12:29:49 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -1,14 +1,5 @@
-import java.util.Scanner;  
-public class Table 
-{  
-public static void main(String[] args)   
-{  
-Scanner sc = new Scanner(System.in);  
-System.out.print("Enter number: ");         
-int num=sc.nextInt();  
-for(int i=1; i <= 10; i++)  
-{        
-System.out.println(num+" * "+i+" = "+num*i);  
-} 
-}
-}
+class Simple{  
+public static void main(String args[]){  
+System.out.println("Hello World");  
+}  
+}  
